d42a6b5c03c32a4f2744c4d79ad35aa3ae5baa7a
Emre Mısırlıoğlu <emremsr95@gmail.com>
2023-01-16 13:51:53 +0300

    1

--- pages/components/ProductSlider.js
+++ pages/components/ProductSlider.js
@@ -1,9 +1,32 @@
 import Image from "next/image";
 import React from "react";
-import { data } from "../../data/data";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 export default function ProductSlider() {
+
+  const data = [
+    {
+      id: 1,
+      img: "/../public/bg-1.png",
+      desc: "365 Signature Hoodie",
+      price: 33.95,
+    },
+    {
+      id: 2,
+      img: "/../public/bg-2.png",
+      desc: "Organic Skinny High Waist Jeans",
+      price: 33.95,
+    },
+    {
+      id: 3,
+      img: "/../public/bg-3.png",
+      desc: "365 Signature Hoodie",
+      price: 33.95,
+    },
+  ];
+  
+
+
   const slideLeft = () => {
     var slider = document.getElementById("slider");
     slider.scrollLeft = slider.scrollLeft - 500;
